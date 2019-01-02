031deebdddbc16bda1b81e8832efb0b6a70491b3
hakki riza kucuk <hakkirizakucuk@yandex.com>
2019-01-02 16:41:02 +0100

    TIDY up ./views/PostTemplate.js

--- src/views/PostTemplate.js
+++ src/views/PostTemplate.js
@@ -1,4 +1,23 @@
 // ./views/PostTemplate.js
+// Anatomy of a Post
+// Later, a post must have capabilities of
+// - rendering mathematical expressions,
+// - rendering graph of a function(s)
+// - rendering highlighted code of many languages
+// - looking readable on devices
+// - having a structure (tags, titles, content itself)
+
+// The aim of this hussle is to be able to create a simple blogging experience
+// with simple Markdown editing, instant live update and deploying by a simple command
+// As for markdown editing, currently, a markdown must be first converted to HTML here,
+// https://markdowntohtml.com/
+// And then converted to m() VNodes here
+// http://arthurclemens.github.io/mithril-template-converter/index.html
+
+// This problem is either need to be addressed,
+// or blogging directly with VNodes must be accepted as a standard
+// For my own part, I currently have a friendly experience with VNodes
+
 const m = require('mithril')
 
 const PostTemplate = {
