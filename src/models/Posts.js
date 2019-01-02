a46143ab16c82dabb9b0fc16554dbd287cd2b491
hakki riza kucuk <hakkirizakucuk@yandex.com>
2019-01-02 16:23:09 +0100

    TIDY up ./models/Posts.js

--- src/models/Posts.js
+++ src/models/Posts.js
@@ -1,8 +1,15 @@
 // ./models/Posts
 // Generate lists of posts and add them to the routes
+// A post is generated through m() so it can be displayed in a 'view' property
+
+// TODO:
+// Here, base and attrs parameters are temporary solutions.
+// They can be found in index.js
+
 const m = require('mithril')
-const PostTemplate = require('../views/PostTemplate')
 const routes = require('./routes');
+const PostTemplate = require('../views/PostTemplate')
+
 const Posts = (base, attrs) => {
   let list = []
   let currentPost = undefined
