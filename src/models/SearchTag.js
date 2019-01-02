45ae44dd9d587c064182ea0ef0e3b34d3947b100
hakki riza kucuk <hakkirizakucuk@yandex.com>
2019-01-02 16:29:13 +0100

    TIDY up ./models/SearchTag.js

--- src/models/SearchTag.js
+++ src/models/SearchTag.js
@@ -1,10 +1,18 @@
 // ./models/SearchTag.js
+// Given an input, return list of posts that includes this input as one of their tag
+// Input can be a tag or many tags seperated by comma
+// If any of the tags satisfy the case, the Post will be added to the list.
+
+
+// TODO:
+// Here, base and attrs parameters are temporary solutions.
+// They can be found in index.js
+
 const m = require('mithril')
 const PostTemplate = require('../views/PostTemplate')
 
 
 const SearchTag = (targetTag, base, attrs) => {
-  //  https://stackoverflow.com/a/39893636
   if(targetTag === '') return undefined
   let targetTags = targetTag.split(',') // split into a list
   let list = []
@@ -18,3 +26,6 @@ const SearchTag = (targetTag, base, attrs) => {
 }
 
 module.exports = SearchTag;
+
+// REFERENCES
+// https://stackoverflow.com/a/39893636
