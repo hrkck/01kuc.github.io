d05b0f48d56e38c2c21c0b8433446d86dba0cd67
hakki riza kucuk <hakkirizakucuk@yandex.com>
2019-01-07 14:24:35 +0100

    TIDY up the code, buld the files.

--- src/views/Graph.js
+++ src/views/Graph.js
@@ -5,17 +5,10 @@ const m = require('mithril')
 const draw = require('../helpers/draw')
 
 const Graph = {
-  oncreate: () => {
-
-  },
-  view: (vnode) =>{
-
-    return (
-        m('div',
-          m('canvas', {id:"", width:'300', height:'300', funcs: vnode.attrs.funcs, oncreate: draw})
-        )
+  view: (vnode) =>
+    m('div',
+      m('canvas', {id:"", width:'300', height:'300', funcs: vnode.attrs.funcs, oncreate: draw})
     )
-  }
 }
 
 module.exports = Graph
