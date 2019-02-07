1d855646a9f0cc1f6e57a1e1ad8036cc4ced6ec9
hakki riza kucuk <hakkirizakucuk@yandex.com>
2019-02-07 22:50:04 +0100

    Updates

--- src/models/state.js
+++ src/models/state.js
@@ -2,15 +2,26 @@
 // Simple State object
 // Currently needed to hold a state when searching for a post by tag
 
-const m = require('mithril')
-
+const posts = require('../../content/all_posts')
 
 let state = {
   searchedTag: '',
-  searchTag: (e) => state.searchedTag = e.target.value,
+  searchTag: (e) => {
+    state.searchedTag = e.target.value
+    state.filterListedTags()
+  },
   
-  searchDisplayType: true,
-  searchDisplayTypeReducer: (boolVal) => state.searchDisplayType = boolVal
+  searchDisplayType: localStorage['isDisplay'] == 'header_post'? true:false,
+  searchDisplayTypeReducer: (boolVal) => state.searchDisplayType = boolVal,
+
+  listedTags: posts.map(post => post.tags).join(',').split(',').filter((v, i, a) => a.indexOf(v) === i),
+  filterListedTags: () => {
+    let all_tags = posts.map(post => post.tags).join(',').split(',').filter((v, i, a) => a.indexOf(v) === i)
+    state.listedTags = []
+    for(let tag of all_tags){
+      if(tag.toLowerCase().includes(state.searchedTag.toLowerCase())) state.listedTags.push(tag)
+    }
+  }
 }
 
 module.exports = state
