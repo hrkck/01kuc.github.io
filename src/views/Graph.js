77aa4776a5ed5bcab4a3e21cdf6db7e31d143f20
hakki riza kucuk <hakkirizakucuk@yandex.com>
2019-02-08 02:22:09 +0100

    Drawing functional graphs improve: ability to update graph in real time, just like any other special function.

--- src/views/Graph.js
+++ src/views/Graph.js
@@ -17,7 +17,7 @@ const style = {
 const Graph = {
   view: (vnode) =>
     m('div',
-      m('canvas', {style: style, id:"", width:'360', height:'360', funcs: vnode.attrs.funcs, oncreate: vnode.attrs.drawFunc, limit: vnode.attrs.limit, step: vnode.attrs.step})
+      m('canvas', {style: style, id:"", width:'360', height:'360', funcs: vnode.attrs.funcs, oncreate:  vnode.attrs.drawFunc, onupdate: vnode.attrs.drawFunc, limit: vnode.attrs.limit, step: vnode.attrs.step})
     )
 }
 
