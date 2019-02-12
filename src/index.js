2992d926b25614cf307c548cb68bd0dc9a9184bf
hakki riza kucuk <hakkirizakucuk@yandex.com>
2019-02-12 14:48:31 +0100

    Updates

--- src/index.js
+++ src/index.js
@@ -7,14 +7,24 @@ const FeaturesPage = require('./views/FeaturesPage')
 const CreatePost = require('./views/CreatePost')
 const Link = require('./views/Link')
 
+const markdown = require('./helpers/renderMarkdown')
 
 const Main = {
   view: () =>
     m('div.container.p-0',
       m('div.container',
-        m('h2', 'Hello World! made in Mithril js'),
-        m('p', 'Lest it becomes as strong as Mithril from Misty Mountains...'),
-        m('p', m(Link, {link: 'CreatePost'}, 'click here to create a post -> ')),
+        [
+          markdown(`##Welcome to my blog!
+          This is where I post stuff I like or demonstrate/document cool things I do as learning projects. This website itself\
+          is one!`),
+          m("p", 
+            "You can search some tags to start reading things. Or check out ", 
+            m('span', m(Link, { link: 'CreatePost' }, 'the coolest page')),
+            " on this website, where I actually\
+            write a post, prototype a page or experiment features."
+          ),
+          markdown(`Find me on [github](https://github.com/archetypum), [twitter](https://twitter.com/hakkirizakucuk) or [linkedin](https://www.linkedin.com/in/hrkucuk/)`)
+        ]
       ),
       m(SearchSection),
     )
