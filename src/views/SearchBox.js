6090ea421c8aee4daba4d83af737a949699c7f6d
hakki riza kucuk <hakkirizakucuk@yandex.com>
2019-02-14 02:11:30 +0100

    Updates

--- src/views/SearchBox.js
+++ src/views/SearchBox.js
@@ -9,7 +9,7 @@ const state = require('../models/state')
 const SearchBox = () => {
   return {
     view: () =>
-      m(".input-group.mb-3",
+      m(".input-group",
         m("input.form-control[aria-describedby='SearchTags'][aria-label='SearchTags'][placeholder='search tags'][type='text']", { value: state.searchedTag, oninput: state.searchTag }),
         m(".input-group-append",
           m("button.btn.btn-outline-secondary[id='SearchTagsCancelButton'][type='button']", { onclick: () => { state.changeSearchedTag('') } }, m.trust("&times;")))
