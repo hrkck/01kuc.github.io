f0c0823838edc81be3565e5db17d7c730b343f5f
hakki riza kucuk <hakkirizakucuk@yandex.com>
2019-02-14 03:10:30 +0100

    Give search box one click more space

--- src/views/SearchSection.js
+++ src/views/SearchSection.js
@@ -12,9 +12,9 @@ const SearchSection = {
   view: () =>
     m("nav.navbar.navbar-light", { style: { "background-color": "#e3f2fd" } },
       m("form.container",
-        m('div.col-lg-3.col-md-6.col-sm-9', m(SearchBox)),
+        m('div.col-lg-4.col-md-6.col-sm-9', m(SearchBox)),
         m('div.col-lg-3.col-md-4.col-sm-6', m(SearchType)),
-        m('div.col-lg-6.col-md-12.container', m(ListTags)),
+        m('div.col-lg-5.col-md-12.container', m(ListTags)),
         m('.mt-2.mb-5'),
         m('div.col.container', SearchTag(state.searchedTag)),
       )
