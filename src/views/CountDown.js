3aeff6d7c92640949a70c0da8a0b338b9b0ac767
hakki riza kucuk <hakkirizakucuk@yandex.com>
2019-03-30 00:13:52 +0100

    Realtime update for countdown page

--- src/views/CountDown.js
+++ src/views/CountDown.js
@@ -11,24 +11,46 @@ const Link = require("./Link")
 
 let deadline = new Date('April 13, 2019 20:10:00');
 let now = new Date();
-let days = Math.floor((deadline-now)/(1000*60*60*24)); // convert miliseconds to degrees
-let hours = Math.floor((deadline-now)/(1000*60*60) % 24);
-let mins = Math.floor((deadline-now)/(1000*60) % 60);
-let secs = Math.floor((deadline-now)/(1000) % 60);
+let remaining = deadline-now
+let days = Math.floor((remaining)/(1000*60*60*24)); // convert miliseconds to degrees
+let hours = Math.floor((remaining)/(1000*60*60) % 24);
+let mins = Math.floor((remaining)/(1000*60) % 60);
+let secs = Math.floor((remaining)/(1000) % 60);
 // let diff = deadline-now
 
 
-const CountDown = {
-  view: () =>
-   m('div.text-center.mt-5', 
-    m('h1','PARTY COUNTDOWN'),
-    m('div.m-5'),
-    m('h3', 'for the ducking 2south rocknrollers'),
-    
-    m('div.m-5'),
 
-    m('h1.text-center', days, ' days ', hours, ' hours ', mins, ' minutes ', secs, ' seconds left.'),
-  )    
+
+const CountDown = () =>   {
+  
+  let startCountDown = () =>{
+    
+    
+  }
+  
+  return {
+    oncreate: ()=>{
+      setInterval(() => {
+        remaining -= 1000
+        days = Math.floor((remaining)/(1000*60*60*24)); // convert miliseconds to degrees
+        hours = Math.floor((remaining)/(1000*60*60) % 24);
+        mins = Math.floor((remaining)/(1000*60) % 60);
+        secs = Math.floor((remaining)/(1000) % 60);
+        m.redraw();
+      }, 1000);
+      
+    },
+    view: () =>
+      m('div.text-center.mt-5', 
+        m('h1','PARTY COUNTDOWN'),
+        m('div.m-5'),
+        m('h3', 'for the ducking 2south rocknrollers'),
+        
+        m('div.m-5'),
+
+        m('h1.text-center', days, ' days ', hours, ' hours ', mins, ' minutes ', secs, ' seconds left.'),
+      )    
+  }
       
 }
 
